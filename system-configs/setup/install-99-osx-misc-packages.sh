40a80dad2a1a64deced8642bc3951287693089d9
Sagar <sagar.momin@gmail.com>
2023-01-12 00:50:51 -0500

    Spacing

--- system-configs/setup/install-99-osx-misc-packages.sh
+++ system-configs/setup/install-99-osx-misc-packages.sh
@@ -4,6 +4,7 @@
 # ============================================================================
 # Don't source script and set -e -o pipefail {{{1
 # ============================================================================
+
 (return 0 2>/dev/null) && sourced=1 || sourced=0
 if [[ $sourced -ne 0 ]]; then
   echo "Don't source this script. Run it directly"
