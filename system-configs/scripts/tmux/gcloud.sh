94e48532e865bd93d930b72f64dde35b54bfcd4f
Sagar <sagar.momin@gmail.com>
2023-02-04 16:09:39 -0500

    deal with the unset case

--- system-configs/scripts/tmux/gcloud.sh
+++ system-configs/scripts/tmux/gcloud.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-if gcloud config get project &>/dev/null; then
-  echo "<gcloud> $(gcloud config get project)"
-fi
+function get_gcloud_project() {
+  local GCLOUD_PROJECT
+  GCLOUD_PROJECT="$(gcloud config get project 2>/dev/null)"
+  if [[ $? -eq 0 && -n $GCLOUD_PROJECT ]]; then
+    echo "<gcloud> $(gcloud config get project)"
+  fi
+}
+
+get_gcloud_project
