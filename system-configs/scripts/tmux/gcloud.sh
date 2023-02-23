0671c0f33e68f35260e7c09bb2c30294c0d261b8
Sagar <sagar.momin@current.com>
2023-02-23 11:32:36 -0500

    printing better when not connected to gcloud or kube

--- system-configs/scripts/tmux/gcloud.sh
+++ system-configs/scripts/tmux/gcloud.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
-
-function get_gcloud_project() {
-  local GCLOUD_PROJECT
-  GCLOUD_PROJECT="$(gcloud config get project 2>/dev/null)"
-  if [[ $? -eq 0 && -n $GCLOUD_PROJECT ]]; then
-    echo "$(gcloud config get project)"
+#
+## [gcloud-get-service-account]  ->  Shorthand
+#  Gets service account, if set
+function gcloud-get-service-account() {
+  local SERVICE_ACCOUNT=
+  SERVICE_ACCOUNT="$(gcloud config get auth/impersonate_service_account  2> /dev/null)"
+  if [[ -z $SERVICE_ACCOUNT ]]; then
+    return 1
+  else
+    echo "${SERVICE_ACCOUNT}"
+    return 0
   fi
 }
 
-get_gcloud_project
+## [gcloud-active-account]  ->  Shorthand
+#  Displays the active account that gcp is auth'd to
+function gcloud-active-account() {
+  gcloud auth list --format=json | jq -r '.[] | select(.status == "ACTIVE") | .account'
+}
+
+## [gcloud-whoami]  ->  Shorthand
+#  Displays the principal in use. Will first check if is a service account
+function gcloud-whoami() {
+  local SERVICE_ACCOUNT=
+  if ! SERVICE_ACCOUNT="$(gcloud-get-service-account)"; then
+    gcloud-active-account
+  else
+    echo "${SERVICE_ACCOUNT}"
+  fi
+}
+
+function print-gcloud-status-for-tmux() {
+  local GCLOUD_PROJECT
+  GCLOUD_PROJECT="$(gcloud config get project 2>/dev/null)"
+  if [[ $? -eq 0 && -n $GCLOUD_PROJECT ]]; then
+    echo "#[bg=colour235]#[fg=colour220]<gcloud> ${GCLOUD_PROJECT} #[fg=colour219]{$(gcloud-whoami)}#[bg=colour233] "
+  fi
+}
+
+print-gcloud-status-for-tmux
--- system-configs/scripts/tmux/kubectl.sh
+++ system-configs/scripts/tmux/kubectl.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-if kubectl config current-context &>/dev/null; then
-  echo "<kubectl>" "$(kubectl config current-context | sed 's/gke_//' | sed 's/_/ | /g')"
-fi
+function print-kubectl-for-tmux() {
+  local CURRENT_CONTEXT=
+  CURRENT_CONTEXT="$(kubectl config current-context 2>/dev/null | sed 's/gke_//' | sed 's/_/ | /g')"
+
+  if [[ $? -eq 0 && -n $CURRENT_CONTEXT ]]; then
+    echo "#[bg=colour235]#[fg=colour229]#[bg=colour235]#[fg=colour229] <kubectl> ${CURRENT_CONTEXT} #[bg=colour233] "
+  fi
+}
+
+print-kubectl-for-tmux
